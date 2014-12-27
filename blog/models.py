f4706e633aeeb988ece1d0887ba845b670f57883
Sohrab <sohrab.gh@gmail.com>
2014-12-27 19:07:58 +0330

    Added Category and Post models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class   Category(models.Model):
+        title = models.CharField(max_length=50)
+
+
+class   Post(models.Model):
+        title = models.CharField(max_length=100)
+        add_datetime = models.DateTimeField()
+        publish_datetime = models.DateTimeField()
+        category = models.ForeignKey(Category, db_column='cat_id')
+        body = models.CharField(max_length=1000)
+
+
+
